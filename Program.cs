deaabf7e0c8c927218ca812f88a072fd4d0293f1
Šimon Juda Hloska <hloska@spsejecna.cz>
2024-09-18 10:28:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyProject_18_09_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
